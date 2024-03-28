ef0f1d6ec37e121e27259f02317b736e93ea0c7b
sachin <sachinp@iitbhilai.ac.in>
2024-03-28 17:43:48 +0530

    basic

--- controllers/pricing.js
+++ controllers/pricing.js
@@ -5,7 +5,8 @@ const calculatePrice = require("../services/priceCalculator");
 router.post("/calculatePrice", async (req, res) => {
   const { zone, organization_id, total_distance, item_type } = req.body;
 
-  const baseDistance = 5;
+  const baseDistance = 5;//All this data have to fetch from the backend 
+  
   const kmPrice = item_type === "perishable" ? 1.5 : 1;
   const fixPrice = 10;
 

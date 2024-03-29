4ef39c4d42788aa4bd295e494aca4ebe7de85a85
sachin <sachinp@iitbhilai.ac.in>
2024-03-29 13:01:18 +0530

    integeration

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require("./app");
+const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3005;
+
+app.use(express.static(path.join(__dirname, "build")));
 
 const Sequelize = require("sequelize");
 const { development } = require("./config");
@@ -23,6 +27,10 @@ sequelize
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
   });
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
